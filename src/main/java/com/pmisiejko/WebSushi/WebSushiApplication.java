6352f6859a71bd09df7b0dca23c7f13fd1c539b3
pmisiejko <pmisiejko@gmail.com>
2020-04-17 18:24:12 +0200

    Init repository

---
+++ src/main/java/com/pmisiejko/WebSushi/WebSushiApplication.java
@@ -0,0 +1,13 @@
+package com.pmisiejko.WebSushi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSushiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSushiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pmisiejko/WebSushi/WebSushiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmisiejko.WebSushi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSushiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
